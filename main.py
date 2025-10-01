d05d1b7c8132021ff6ec311880a7c6b88e2ea517
Dannyyari <danny_yari@yahoo.com>
2025-10-01 10:44:14 +0200

    justering av contact_api

--- main.py
+++ main.py
@@ -9,25 +9,41 @@ CORS(app, origins=["https://danny-portfolio-devops.netlify.app"], supports_crede
 
 @app.route("/api/contact", methods=["POST", "OPTIONS"])
 def contact_api():
-    if request.method == "OPTIONS":
+
+    if request.method == "OPTIONS":  # Preflight request från frontend
         return '', 200
-    try:
+
+    data = request.get_json()
+    print("Data received:", data)
+
+    return jsonify({"status": "success", "message": "Contact form submitted successfully!"}), 200
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Railway tilldelar port via miljövariabel
+    app.run(host="0.0.0.0", port=port)
+
+
+    #if request.method == "OPTIONS":
+    #    return '', 200
+    #try:
     
-        data = request.get_json()
-        print("Data received:", data)
+     #   data = request.get_json()
+      #  print("Data received:", data)
         
-        if not data:
-            return jsonify({"status": "error", "message": "No JSON data received"}), 400
+       # if not data:
+        #    return jsonify({"status": "error", "message": "No JSON data received"}), 400
         
-        required_fields = ["name", "email", "subject", "message"]
-        for field in required_fields:
-            if field not in data:
-                return jsonify({"status": "error", "message": f"Missing field: {field}"}), 400
-        return jsonify({"status": "success", "message": "Contact form submitted successfully!"})
+        #required_fields = ["name", "email", "subject", "message"]
+        #for field in required_fields:
+        #    if field not in data:
+        #        return jsonify({"status": "error", "message": f"Missing field: {field}"}), 400
+        #return jsonify({"status": "success", "message": "Contact form submitted successfully!"})
         
-    except Exception as e:
-        print("Error:", str(e))
-        return jsonify({"status": "error", "message": "Server error"}), 500
+   # except Exception as e:
+    #    print("Error:", str(e))
+     #   return jsonify({"status": "error", "message": "Server error"}), 500
 
 #@app.route("/api/contact", methods=["POST", "OPTIONS"])
 #@cross_origin(origins=["https://danny-portfolio-devops.netlify.app"])
@@ -49,11 +65,6 @@ def contact_api():
    # except Exception as e:  # Denna rad måste ha samma indentation som try
    #     print("Error:", str(e))
     #    return jsonify({"status": "error", "message": "Server error"}), 500
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Railway tilldelar port via miljövariabel
-    app.run(host="0.0.0.0", port=port)
-
 
 
     #if request.method == "OPTIONS":
